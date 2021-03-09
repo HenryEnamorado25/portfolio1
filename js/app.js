d45cbfa870ed0d6e5873197dc84f2fc2238873bd
Henry Enamorado <henryenamo@gmail.com>
2021-03-09 11:58:49 -0500

    Nav Bar Done

--- js/app.js
+++ js/app.js
@@ -4,7 +4,9 @@ const contact = document.querySelector("#contact");
 const pjSection = document.querySelector(".projectsSection");
 const aboutSection = document.querySelector(".aboutMe");
 const contactSection = document.querySelector(".contactContainer");
-const btn = document.querySelector("#projectBtn")
+const btn = document.querySelector("#projectBtn");
+const navBar = document.querySelector('#navBar');
+const list = document.querySelector(".list");
 
 about.addEventListener("click", () => {
     aboutSection.scrollIntoView();
@@ -17,4 +19,28 @@ contact.addEventListener("click", () => {
 })
 btn.addEventListener('click', () => {
     pjSection.scrollIntoView();
+})
+
+
+var active = true;
+navBar.addEventListener('click', () => {
+    if (active) {
+        active = false;
+        navBar.classList.toggle("fa-times");
+        navBar.style.color = "black";
+
+        list.style.right = "-10px";
+        list.style.transition = "0.5s"
+        list.style.zIndex= "3";
+
+       active = false;        
+        
+    }else {
+        navBar.classList.toggle("fa-times");
+        list.style.right = "-100%";
+        list.style.transition = "0.5s"
+        active = true;
+        
+        
+    }
 })
